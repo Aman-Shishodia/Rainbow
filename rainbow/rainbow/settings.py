439e60356dcd321caebc7234c9464979586a3afa
Aman-Shishodia <amanshishodia2021@gmail.com>
2024-04-29 16:19:31 +0530

    first commit

--- rainbow/rainbow/settings.py
+++ rainbow/rainbow/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bh@i2wdsf7!ks$oii^s_ar^^5tndw2zpc6u=9tns%#8@f$uxut'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
